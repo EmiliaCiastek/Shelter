677613687f6a714bef26e9a651541695251288b1
Emilia <emciastek@gmail.com>
2018-02-23 15:33:19 +0100

    Make test passed

--- src/main/java/com/ciastek/javaacademy/shelter/domain/repository/impl/DatabaseAnimalRepository.java
+++ src/main/java/com/ciastek/javaacademy/shelter/domain/repository/impl/DatabaseAnimalRepository.java
@@ -4,7 +4,6 @@ import com.ciastek.javaacademy.shelter.domain.Animal;
 import com.ciastek.javaacademy.shelter.domain.repository.AnimalRepository;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -13,18 +12,20 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-//@Transactional
+@Transactional
 public class DatabaseAnimalRepository implements AnimalRepository {
-    //@Autowired
-    private SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public DatabaseAnimalRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public List<Animal> getAllAnimals() {
-        Session session = sessionFactory.openSession();
-        List<Animal> animals = session.createQuery("from Animal", Animal.class).list();
-        session.close();
-
-        return animals;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from Animal", Animal.class).list();
     }
 
     @Override
--- src/test/java/com/ciastek/javaacademy/shelter/domain/repository/impl/DatabaseAnimalRepositoryTest.java
+++ src/test/java/com/ciastek/javaacademy/shelter/domain/repository/impl/DatabaseAnimalRepositoryTest.java
@@ -2,23 +2,25 @@ package com.ciastek.javaacademy.shelter.domain.repository.impl;
 
 import com.ciastek.javaacademy.shelter.domain.Animal;
 import com.ciastek.javaacademy.shelter.domain.repository.AnimalRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ContextConfiguration("/test-DispatcherServlet-context.xml")
+@ExtendWith(SpringExtension.class)
 class DatabaseAnimalRepositoryTest {
+
+    @Autowired
     private AnimalRepository repository;
 
-    @BeforeEach
-    public void setUp(){
-        repository = new DatabaseAnimalRepository();
-    }
-
     @Test
-    public void should_return_empty_list_when_db_is_empty(){
+    public void should_return_empty_list_when_db_is_empty() {
         List<Animal> animals = repository.getAllAnimals();
 
         assertThat(animals).isEmpty();
